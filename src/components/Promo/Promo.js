ea0e3d956f8bc140a675a51b0840f8962119525f
javiercubas <66169702+javiercubas@users.noreply.github.com>
2022-07-14 20:42:46 +0200

    update

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -8,18 +8,12 @@ export default function Promo() {
         <section className="promo" id="landing">
             <img src="./assets/foto1.jpg" className='bg-promo'/>
             <div className="contenido-promo">
-                <h2 className="titulo-promo">SPORTING CLUB MADRID
-                    <h2 className='titulo2-promo'>{t("home-promo.title2")}.</h2>
+                <h2 className="titulo-promo">are you ready<br/>to
+                    <h2 className='titulo2-promo'> change</h2>
+                    <h2 className="titulo-promo"> your<br/></h2>
+                    <h2 className='titulo2-promo'>life?</h2>
                 </h2>
-                <div className="botones">
-                    <a href="https://apps.apple.com/us/app/festo-app/id1555730816">
-                        <img style={{ height: "70px" }} src="https://static.festea.party/web/app-store-badge-es.svg" alt="Apple AppStore Badge" />
-                    </a>
-
-                    <a href="https://play.google.com/store/apps/details?id=com.negativeepsilon.festo">
-                        <img style={{ height: "70px" }} src="https://static.festea.party/web/google-play-badge-es.png.webp" alt="Google Play Store Badge" />
-                    </a>
-                </div>
+                <button className='button-promo'>Apply Now</button>
             </div>
 
         </section>
