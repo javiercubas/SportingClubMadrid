0e0e735d1205d9afee91db3d9f133734cd3049c3
javiercubas <javiercubas2003@gmail.com>
2022-07-29 21:36:37 +0200

    update

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,8 +6,7 @@ export default function Promo() {
     const [t] = useTranslation("global");
     return (
         <section className="promo" id="landing">
-            <img src="./assets/foto1.jpg" className='bg-promo'/>
-            <img src="./assets/foto2.jpg" className='bg-promo2'/>
+            <div className='bg-promo'/>
             <div className="contenido-promo">
                 <h2 className="titulo-promo">are you ready<br/>to
                     <h2 className='titulo2-promo'> change</h2>
