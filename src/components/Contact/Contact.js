7bc864075102e79fd8daebcb6cfe73837470cacc
Mario Gutiérrez <102418143+mariogutierrezlopez@users.noreply.github.com>
2022-07-31 21:05:12 +0200

    form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,4 @@
 import './Contact.css'
-
 export default function Contact() {
     return (
       <div class="contact">
@@ -9,34 +8,34 @@ export default function Contact() {
         <hr class="contact__divider"/>
           <div class="contact__main">
             <div class="contact__main__form">
-              <h3 class="contact__form__title">Fill our contact form</h3>
+              <h3 class="contact__form__title"><i class="fa-solid fa-users"></i> Fill our contact form</h3>
               <hr class="contact__form__divider"/>
               <form action="" class="contact__form__form" >
-                <label class="contact__form__form__element">Name: <input type="text" name="name" id="name"/></label>
-                <label class="contact__form__form__element">Email: <input type="email" name="mail" id="mail"/></label>
-                <label class="contact__form__form__element">Phone: <input type="number" name="name" id="name"/></label>
-                <label class="contact__form__form__element">Position: 
-                <select name="" id="">
-                  <option value="">Pick a position</option>
+                <label class="contact__form__form__element"><i class="fa-solid fa-user"></i>Name: <input type="text" name="name" id="name" className='contact__input'/></label>
+                <label class="contact__form__form__element"><i class="fa-solid fa-at"></i>Email: <input type="email" name="mail" id="mail" className='contact__input'/></label>
+                <label class="contact__form__form__element"><i class="fa-solid fa-phone"></i>Phone: <input type="number" name="name" id="name" className='contact__input'/></label>
+                <label class="contact__form__form__element"><i class="fa-solid fa-futbol"></i>Position: 
+                <select name="" id="" className='contact__input'>
+                  <option value="" disabled selected>Pick a position</option>
                   <option value="1">Goalie</option>
                   <option value="2">Defender</option>
                   <option value="3">Midfield</option>
                   <option value="4">Fordward/Winger</option>
                 </select>
                 </label>
-                <label class="contact__form__form__element">Age: <input min={14} type="number" name="age" id="age"/></label>
+                <label class="contact__form__form__element"><i class="fa-solid fa-person"></i>Age: <input min={14} type="number" name="age" id="age" className='contact__input'/></label>
                 
-                <label class="contact__form__form__element">Degree, gap year or Semester abroad?</label>
-                <label class="contact__form__form__element"><select name="" id="">
-                  <option value="">Choose an option</option>
+                <label class="contact__form__form__element"><i class="fa-solid fa-school"></i>Degree, gap year or Semester abroad?<select name="" id="select__input" className='contact__input'>
+                  <option value="" disabled selected>Choose an option</option>
                   <option value="1">Undergraduate degree</option>
                   <option value="2">Masters degree</option>
                   <option value="3">Gap year</option>
                   <option value="4">Semester abroad</option>
                   <option value="5">Undecided</option>
                 </select></label>
-                <label class="contact__form__form__element">Previous Club<input type="text" name="prev_club" id="prev_club"/></label>
-                <input class="contact__form__form__element" type="submit" value="Submit"/>
+                <label class="contact__form__form__element"><i class="fa-solid fa-people-group"></i>Previous Club<input type="text" name="prev_club" id="prev_club" className='contact__input'/></label>
+                <div class="contact__form__form__--submit"><button id="submit" class="contact__submit" type="submit" value="Submit">SUBMIT
+                </button></div>
               </form>
             </div>
             <div class="contact__main__image"></div>
