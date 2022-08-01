22f6ccf49bf5dedecc1dfac0827865ec09baa241
javiercubas <javiercubas2003@gmail.com>
2022-08-01 17:33:30 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import CarruselOpciones from '../components/CarruselOpciones/CarruselOpciones';
-import Carrusel from '../components/Carrusel/Carrusel';
-import TOTW from '../components/TOTW/TOTW';
 import Promo from '../components/Promo/Promo';
-import EquiposHome from '../components/EquiposHome/EquiposHome'
-import WebStats from '../components/WebStats/WebStats';
-import TOTW2 from '../components/TOTW/TOTW2';
 import CardMatch from '../components/Partidos/cardMatch.js';
 import './Home.css';
 import TablaClasificacion from '../components/TablaClasificacion/TablaClasificacion';
-import { countDown } from '../components/Partidos/Timer/countDown';
 import Contact from '../components/Contact/Contact';
 import TwitterTimeline from '../components/TwitterTimeline/TwitterTimeline';
 import Partners from '../components/Partners/Partners';
@@ -71,7 +64,7 @@ export default function Home() {
                 <TablaClasificacion/>
                 <TablaClasificacion/>
             </div>
-            
+            <Contact/>
             {/*<TwitterTimeline/>*/}
         </>
     )
