6d43fee8f7b49d21710d3e607e9b95fba5dc75dc
Mario Gutiérrez <102418143+mariogutierrezlopez@users.noreply.github.com>
2022-08-04 16:13:23 +0200

    dwadwa

--- src/pages/ClubStaff.js
+++ src/pages/ClubStaff.js
@@ -3,11 +3,11 @@ import './ClubStaff.css';
 export default function ClubStaff(){
     return(
         <div class="clubStaff">
-            <div class="clubStaff__men">
-                <h2 className='men__title'>Coaching Men's Staff</h2>
+            <div class="clubStaff__div">
+                <h2 className='div__title'>Coaching Men's Staff</h2>
                 <hr class="clubStaff__hr"/>
-                <div class="men__grid">
-                    <div class="men__grid__card">
+                <div class="div__grid">
+                    <div class="div__grid__card">
                         <div class="card__person">
                             <h3 class="person__title">Alex Sánchez</h3>
                             <div class="person__photo"></div>
@@ -25,41 +25,126 @@ export default function ClubStaff(){
                             </ul>
                         </div>
                     </div>
-                    <div class="men__grid__card">
+                    <div class="div__grid__card">
                         
-                        <div class="card__info2">
+                        <div class="card__info2" dir="RTL">
                             <ul class="info__list">
                                 <li>UEFA 'Pro' License</li>
-                                <li>PhD candidate Sports Psychology, Master's in Cognitive Neuroscience and Pshychobiology, Official Master in Sport Psychology Research</li>
-                                <li>BS Sports and Physical Activity Science</li>
-                                <li>Director of the International Masters in Football </li>
-                                <li>Coaching and Management program at Real Madrid </li>
-                                <li>Postgraduate School</li>
-                                <li>Former Methodology Director at Getafe CF, Former Coach Alcorcon AD's U-18, Experience coaching in Spain, Italy, Angola and Oman</li>
+                                <li>Head Coach of CDC Moscardo 'B' in Segunda Regional senior division</li>
+                                <li>Head Coach of CDC Moscardo Infantil U-16</li>
+                                <li>7 years of experience coaching in Spain</li>
                             </ul>
                         </div>
                         <div class="card__person">
-                            <h3 class="person__title">Alex Sánchez</h3>
+                            <h3 class="person__title">José Santos</h3>
                             <div class="person__photo"></div>
-                            <h3 class="person__task">Head coach</h3>
+                            <h3 class="person__task">Assistant Coach</h3>
                         </div>
                     </div>
-                    <div class="men__grid__card">
+                    <div class="div__grid__card">
                         <div class="card__person">
-                            <h3 class="person__title">Alex Sánchez</h3>
+                            <h3 class="person__title">Fabián Pozo</h3>
+                            <div class="person__photo"></div>
+                            <h3 class="person__task">Conditioning Coach</h3>
+                        </div>
+                        <div class="card__info">
+                            <ul class="info__list">
+                                <li>FC Barcelona Innovation Hub</li>
+                                <li>Strength training and performance rehab, Training Methodology, What to train </li>
+                                <li>MBA Sports Management Universidad Europea Madrid</li>
+                                <li>Coaching experience at CD Mostoles</li>
+                                <li>Certified Personal trainer</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+            <div class="clubStaff__div">
+                <h2 className='div__title'>Coaching Women's Staff</h2>
+                <hr class="clubStaff__hr"/>
+                <div class="div__grid">
+                    <div class="div__grid__card">
+                        <div class="card__person">
+                            <h3 class="person__title">Alex Ferreira</h3>
                             <div class="person__photo"></div>
                             <h3 class="person__task">Head coach</h3>
                         </div>
                         <div class="card__info">
                             <ul class="info__list">
-                                <li>UEFA 'Pro' License</li>
-                                <li>PhD candidate Sports Psychology, Master's in Cognitive Neuroscience and Pshychobiology, Official Master in Sport Psychology Research</li>
-                                <li>BS Sports and Physical Activity Science</li>
-                                <li>Director of the International Masters in Football </li>
-                                <li>Coaching and Management program at Real Madrid </li>
-                                <li>Postgraduate School</li>
-                                <li>Former Methodology Director at Getafe CF, Former Coach Alcorcon AD's U-18, Experience coaching in Spain, Italy, Angola and Oman</li>
+                                <li>Master's candidate in International Football Coach and Management from Escuela Universitaria Real Madrid</li>
+                                <li>USSF'D' License</li>
+                                <li>Coaching Experience at the Senior level with Madrid CFF (Women's First Division)</li>
+                                <li>Youth coaching at CD Canillas, North Huntington Beach FC, Earlham College, and Mater Dei HS </li>
                             </ul>
+                        </div>
+                    </div>
+                    <div class="div__grid__card">
+                        
+                        <div class="card__info2" dir="RTL">
+                            <ul class="info__list">
+                                <li>UEFA 'A' License, PhD Women's Football development</li>
+                                <li>Coaching experience at Madrid CFF & Union Zona Norte</li>
+                                <li>Many experiences in Director roles for Entrepreneurial oriented programs</li>
+                                <li>As a Program Specialist, developed several project in Qatar for the 2022 World Cup</li>
+                            </ul>
+                        </div>
+                        <div class="card__person">
+                            <h3 class="person__title">Rachel Esteban</h3>
+                            <div class="person__photo"></div>
+                            <h3 class="person__task">Assitant Coach</h3>
+                        </div>
+                    </div>
+                    <div class="div__grid__card">
+                        <div class="card__person">
+                            <h3 class="person__title">Fabián Pozo</h3>
+                            <div class="person__photo"></div>
+                            <h3 class="person__task">Conditioning Coach</h3>
+                        </div>
+                        <div class="card__info">
+                            <ul class="info__list">
+                                <li>EUFA 'Pro' License, Master's in Coaching Methodology</li>
+                                <li>B.S. Exercise Science</li>
+                                <li>Head and Assistant Coach for AD Villaviciosa de Odon (3ª RFEF) and CD Valle de Esgüés (3ª RFEF)</li>
+                                <li>Real Madrid Foundation Head Coach in the USA</li>
+                                <li>FC Barcelona's training academy in Denmark. Developer of the Female BSF (Danish Female Superleague) Training Methodoly for the Youth Team</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clubStaff__div">
+                <h2 className='div__title'>Aditional Staff</h2>
+                <hr class="clubStaff__hr"/>
+                <div class="div__grid">
+                    <div class="div__grid__card">
+                        <div class="card__person">
+                            <h3 class="person__title">Alex Ferreira</h3>
+                            <div class="person__photo"></div>
+                            <h3 class="person__task">Head coach</h3>
+                        </div>
+                        <div class="card__info">
+                            <ul class="info__list">
+                                <li>B.S Exercise Science at Universidad Europea Madrid</li>
+                                <li>Coaching experience at AD Alcorcon and Tres Cantos CF</li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="div__grid__card">
+                        
+                        <div class="card__info2" dir="RTL">
+                            <ul class="info__list">
+                                <li>B.S Exercise Science and Physical Therapy at Universidad Europea de Madrid</li>
+                                <li>Expertise in Manual Therapies and Dry Needling</li>
+                                <li>Physiotherapist experience in CD Internacional de Móstoles</li>
+                                <li>Conditioning coach in Getafe CF</li>
+                                <li>Coach at Fundación Atlético Madrid</li>
+                            </ul>
+                        </div>
+                        <div class="card__person">
+                            <h3 class="person__title">Rachel Esteban</h3>
+                            <div class="person__photo"></div>
+                            <h3 class="person__task">Assitant Coach</h3>
                         </div>
                     </div>
                 </div>
