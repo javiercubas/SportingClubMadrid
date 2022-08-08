cbdc00ddf6d375b6ddd0a6d125fac2a657aa09b6
Mario Gutiérrez <102418143+mariogutierrezlopez@users.noreply.github.com>
2022-08-08 13:42:35 +0200

    r3wr3w

---
+++ src/components/StaffCard/StaffCard.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react";
+import './StaffCard.css';
+export default function StaffCard(props){
+    if(props.type==1)
+        return(<Fragment>
+            <div class="div__grid__card">
+                <div class="card__person">
+                    <h3 class="person__title">{props.name}</h3>
+                    <div class="person__photo"></div>
+                    <h3 class="person__task">{props.task}</h3>
+                </div>
+                <div class="card__info">
+                    <ul class="info__list">
+                        {props.lista.map((task)=>(<li>{task}</li>))}
+                    </ul>
+                </div>
+            </div>
+        </Fragment>);
+    else
+        return(<Fragment>
+            <div class="div__grid__card">
+                <div class="card__info2">
+                    <ul class="info__list">
+                        {props.lista.map((task)=>(<li>{task}</li>))}
+                    </ul>
+                </div>
+                <div class="card__person">
+                    <h3 class="person__title">{props.name}</h3>
+                    <div class="person__photo"></div>
+                    <h3 class="person__task">{props.task}</h3>
+                </div>
+            </div>
+        </Fragment>);
+}
